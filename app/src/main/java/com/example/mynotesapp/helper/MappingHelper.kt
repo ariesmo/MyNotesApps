135acecf1c4802b19f40e8b62d618dae51f830d8
ariesmo <abiesah@gmail.com>
2021-08-05 21:42:39 +0700

    note app android

---
+++ app/src/main/java/com/example/mynotesapp/helper/MappingHelper.kt
@@ -0,0 +1,22 @@
+package com.example.mynotesapp.helper
+
+import android.database.Cursor
+import com.example.mynotesapp.Note
+import com.example.mynotesapp.db.DatabaseContract
+
+object MappingHelper {
+    fun mapCursorToArrayList(noteCursor: Cursor?) : ArrayList<Note>{
+        val noteList = ArrayList<Note>()
+
+        noteCursor?.apply {
+            while (moveToNext()){
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                noteList.add(Note(id, title, description, date))
+            }
+        }
+        return noteList
+    }
+}
